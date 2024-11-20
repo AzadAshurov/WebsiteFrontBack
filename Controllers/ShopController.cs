2836cdcc4e6b6f3298902d4c3d4973e794ca0bbc
Wertdrof <wertdrof@gmail.com>
2024-11-20 23:55:52 +0400

    Add project files.

---
+++ Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            return View();
+        }
+    }
+}
