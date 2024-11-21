4ba86e82b6775f5572b4e85a81d60a29b8014b74
Wertdrof <wertdrof@gmail.com>
2024-11-21 11:10:33 +0400

    Working version

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
 
 namespace WebApplication1.Controllers
 {
@@ -6,6 +8,41 @@ namespace WebApplication1.Controllers
     {
         public IActionResult Index(int id)
         {
+            List<Slide> slides = new List<Slide>
+            {
+                new Slide
+                {
+                    Id = 1,
+                    Title = "Very interesting title",
+                    Description="Very cool, my uncle liked it",
+                    SubTitle = "Very interesting subtitle",
+                    Image = "e5f8d96c-3e16-4558-b633-c921c71228f8-1716396008555.jpg",
+                    Order = 1
+                },
+                   new Slide
+                {
+                    Id = 2,
+                    Title = "Very lorem ipsum dolor sit amet title",
+                    Description="Very cool, I liked it",
+                    SubTitle = "Very interesting subtitle",
+                    Image = "angry.png",
+                    Order = 3
+                },
+                        new Slide
+                {
+                    Id = 2,
+                    Title = "Very interesting title",
+                    Description="Very cool, all mexican cartel liked it",
+                    SubTitle = "Very interesting subtitle",
+                    Image = "Demonicicon.png",
+                    Order = 2
+                }
+            };
+
+            HomeVM homeVM = new HomeVM
+            {
+                Slides = slides
+            };
             return View();
         }
     }
