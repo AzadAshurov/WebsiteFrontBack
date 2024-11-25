35f8050cf2dbf87d512d85894f70caa57dc782bc
Wertdrof <wertdrof@gmail.com>
2024-11-25 11:04:07 +0400

    Admin panel added, category panel added, need for creating category

---
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.DAL;
+using WebApplication1.Models;
+
+namespace WebApplication1.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Category> categories = await _context.Category.Include(x => x.Product).ToListAsync();
+            return View(categories);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,23 +3,27 @@ using WebApplication1.DAL;
 
 namespace WebApplication1
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-			builder.Services.AddControllersWithViews();
-			builder.Services.AddDbContext<AppDbContext>(opt =>
-			opt.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
-			var app = builder.Build();
-			app.UseStaticFiles();
-			app.MapControllerRoute(
-				name: "default",
-				pattern: "{controller=Home}/{action=Index}/{id?}"
-			);
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<AppDbContext>(opt =>
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+            var app = builder.Build();
+            app.UseStaticFiles();
+            app.MapControllerRoute(
+               name: "admin",
+               pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+           );
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}"
+            );
 
-			app.Run();
-		}
-	}
+            app.Run();
+        }
+    }
 }
