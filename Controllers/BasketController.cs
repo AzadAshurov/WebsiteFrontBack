ff650528467a07c0e4af8250bbc3fa6ff4cc9eab
Wertdrof <wertdrof@gmail.com>
2024-12-13 12:31:33 +0400

    Bag for logined user added

--- Controllers/BasketController.cs
+++ Controllers/BasketController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using WebApplication1.Areas.Admin.Models;
 using WebApplication1.DAL;
 using WebApplication1.Models;
 using WebApplication1.ViewModels;
@@ -11,44 +14,62 @@ namespace WebApplication1.Controllers
     public class BasketController : Controller
     {
         private readonly AppDbContext _context;
+        private UserManager<AppUser> _userManager;
 
-        public BasketController(AppDbContext context)
+        public BasketController(AppDbContext context, UserManager<AppUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public async Task<IActionResult> Index()
         {
-            List<BasketCookieItemVM> cookiesVM;
-            string cookie = Request.Cookies["basket"];
-
             List<BasketItemVM> basketVM = new();
-
-            if (cookie == null)
+            if (!User.Identity.IsAuthenticated)
             {
-                return View(basketVM);
-            }
+                List<BasketCookieItemVM> cookiesVM;
+                string cookie = Request.Cookies["basket"];
 
-            cookiesVM = JsonConvert.DeserializeObject<List<BasketCookieItemVM>>(cookie);
 
-            foreach (BasketCookieItemVM item in cookiesVM)
-            {
-                Product product = await _context.Products.Include(p => p.ProductImages.Where(pi => pi.IsPrimary == true)).FirstOrDefaultAsync(p => p.Id == item.Id);
-                if (product != null)
+
+                if (cookie == null)
                 {
-                    basketVM.Add(new BasketItemVM
+                    return View(basketVM);
+                }
+
+                cookiesVM = JsonConvert.DeserializeObject<List<BasketCookieItemVM>>(cookie);
+
+                foreach (BasketCookieItemVM item in cookiesVM)
+                {
+                    Product product = await _context.Products.Include(p => p.ProductImages.Where(pi => pi.IsPrimary == true)).FirstOrDefaultAsync(p => p.Id == item.Id);
+                    if (product != null)
                     {
-                        Id = product.Id,
-                        Name = product.Name,
-                        Image = product.ProductImages[0].Image,
-                        Price = product.Price,
-                        Count = item.Count,
-                        SubTotal = item.Count * product.Price
-                    });
+                        basketVM.Add(new BasketItemVM
+                        {
+                            Id = product.Id,
+                            Name = product.Name,
+                            Image = product.ProductImages[0].Image,
+                            Price = product.Price,
+                            Count = item.Count,
+                            SubTotal = item.Count * product.Price
+                        });
+                    }
                 }
             }
-
-
+            else
+            {
+                basketVM = _context.BasketItems
+                 .Where(bi => bi.AppUserId == User.FindFirstValue(ClaimTypes.NameIdentifier))
+                 .Select(bi => new BasketItemVM
+                 {
+                     Count = bi.Count,
+                     Price = bi.Product.Price,
+                     Image = bi.Product.ProductImages.FirstOrDefault(pi => pi.IsPrimary == true).Image,
+                     Name = bi.Product.Name,
+                     SubTotal = bi.Product.Price * bi.Count,
+                     Id = bi.ProductId
+                 }).ToList();
+            }
             return View("/Views/Basket/Index.cshtml", basketVM);
         }
 
@@ -59,41 +80,65 @@ namespace WebApplication1.Controllers
 
             bool result = await _context.Products.AnyAsync(p => p.Id == id);
             if (!result) return NotFound();
-
-            List<BasketCookieItemVM> basket;
-            string cookies = Request.Cookies["basket"];
-            if (cookies != null)
+            if (!User.Identity.IsAuthenticated)
             {
-                basket = JsonConvert.DeserializeObject<List<BasketCookieItemVM>>(cookies);
-                BasketCookieItemVM existed = basket.FirstOrDefault(b => b.Id == id);
-
-                if (existed != null)
+                List<BasketCookieItemVM> basket;
+                string cookies = Request.Cookies["basket"];
+                if (cookies != null)
                 {
-                    existed.Count++;
+                    basket = JsonConvert.DeserializeObject<List<BasketCookieItemVM>>(cookies);
+                    BasketCookieItemVM existed = basket.FirstOrDefault(b => b.Id == id);
+
+                    if (existed != null)
+                    {
+                        existed.Count++;
+                    }
+                    else
+                    {
+                        basket.Add(new BasketCookieItemVM()
+                        {
+                            Id = id.Value,
+                            Count = 1
+                        });
+                    }
                 }
                 else
                 {
+                    basket = new List<BasketCookieItemVM>();
+
                     basket.Add(new BasketCookieItemVM()
                     {
                         Id = id.Value,
                         Count = 1
                     });
                 }
+
+                string json = JsonConvert.SerializeObject(basket);
+                Response.Cookies.Append("basket", json);
             }
             else
             {
-                basket = new List<BasketCookieItemVM>();
+                AppUser? user = await _userManager.Users
+                      .Include(u => u.BasketItems)
+                     .FirstOrDefaultAsync(u => u.Id == User.FindFirstValue(ClaimTypes.NameIdentifier));
 
-                basket.Add(new BasketCookieItemVM()
+                BasketItem item = user.BasketItems.FirstOrDefault(bi => bi.ProductId == id);
+
+                if (item is null)
                 {
-                    Id = id.Value,
-                    Count = 1
-                });
+                    user.BasketItems.Add(new BasketItem
+                    {
+                        ProductId = id.Value,
+                        Count = 1,
+                    });
+                }
+                else
+                {
+                    item.Count++;
+                }
+
+                await _context.SaveChangesAsync();
             }
-
-            string json = JsonConvert.SerializeObject(basket);
-            Response.Cookies.Append("basket", json);
-
             return RedirectToAction("Index", "Home");
         }
 
--- Services/Implementations/LayoutService.cs
+++ Services/Implementations/LayoutService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using WebApplication1.DAL;
 using WebApplication1.Models;
@@ -11,52 +12,72 @@ namespace WebApplication1.Services.Implementations
     {
         private readonly AppDbContext _context;
         private readonly HttpContext _http;
+        private readonly ClaimsPrincipal _user;
 
         public LayoutService(AppDbContext context, IHttpContextAccessor http)
         {
             _context = context;
             _http = http.HttpContext;
+            _user = _http.User;
         }
 
         public async Task<List<BasketItemVM>> GetBasketAsync()
         {
-
-            List<BasketCookieItemVM> cookiesVM;
-            string cookie = _http.Request.Cookies["basket"];
-
             List<BasketItemVM> basketVM = new();
 
-            if (cookie == null)
+            if (_user.Identity.IsAuthenticated)
             {
-                return basketVM;
-            }
-
-            cookiesVM = JsonConvert.DeserializeObject<List<BasketCookieItemVM>>(cookie);
-
-            foreach (BasketCookieItemVM item in cookiesVM)
-            {
-                Product product = await _context.Products
-                    .Include(p => p.ProductImages.Where(pi => pi.IsPrimary == true))
-                    .FirstOrDefaultAsync(p => p.Id == item.Id);
-
-                if (product != null)
+                if (_context.BasketItems is not null)
                 {
-                    basketVM.Add(new BasketItemVM
-                    {
-                        Id = product.Id,
-                        Name = product.Name,
-                        Image = product.ProductImages[0].Image,
-                        Price = product.Price,
-                        Count = item.Count,
-                        SubTotal = item.Count * product.Price
-                    });
+                    basketVM = _context.BasketItems
+                       .Where(bi => bi.AppUserId == _user.FindFirstValue(ClaimTypes.NameIdentifier))
+                       .Select(bi => new BasketItemVM
+                       {
+                           Count = bi.Count,
+                           Price = bi.Product.Price,
+                           Image = bi.Product.ProductImages.FirstOrDefault(pi => pi.IsPrimary == true).Image,
+                           Name = bi.Product.Name,
+                           SubTotal = bi.Product.Price * bi.Count,
+                           Id = bi.ProductId
+                       })
+                       .ToList();
                 }
             }
+            else
+            {
+                List<BasketCookieItemVM> cookiesVM;
+                string cookie = _http.Request.Cookies["basket"];
 
 
+                if (cookie == null)
+                {
+                    return basketVM;
+                }
+
+                cookiesVM = JsonConvert.DeserializeObject<List<BasketCookieItemVM>>(cookie);
+
+                foreach (BasketCookieItemVM item in cookiesVM)
+                {
+                    Product product = await _context.Products
+                        .Include(p => p.ProductImages.Where(pi => pi.IsPrimary == true))
+                        .FirstOrDefaultAsync(p => p.Id == item.Id);
+
+                    if (product != null)
+                    {
+                        basketVM.Add(new BasketItemVM
+                        {
+                            Id = product.Id,
+                            Name = product.Name,
+                            Image = product.ProductImages[0].Image,
+                            Price = product.Price,
+                            Count = item.Count,
+                            SubTotal = item.Count * product.Price
+                        });
+                    }
+                }
+            }
             return basketVM;
         }
-
         public async Task<Dictionary<string, string>> GetSettingAsync()
         {
             Dictionary<string, string> settings = await _context.Settings.ToDictionaryAsync(x => x.Key, x => x.Value);
